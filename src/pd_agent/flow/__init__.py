94da179fc5fe186fe68e5aa6af5d61b870ebb167
NithinRajKore <nithinrajkore1997@gmail.com>
2026-04-16 18:38:35 -0700

    feat(flow): add OpenLaneRunner with nix-shell auto-detection

--- src/pd_agent/flow/__init__.py
+++ src/pd_agent/flow/__init__.py
@@ -1,5 +1,16 @@
 """Flow orchestration: invoke OpenLane and parse results."""
 
 from pd_agent.flow.models import FlowMetrics, RunResult
+from pd_agent.flow.runner import (
+    InvocationMode,
+    OpenLaneRunner,
+    RunnerNotAvailableError,
+)
 
-__all__ = ["FlowMetrics", "RunResult"]
+__all__ = [
+    "FlowMetrics",
+    "InvocationMode",
+    "OpenLaneRunner",
+    "RunResult",
+    "RunnerNotAvailableError",
+]
